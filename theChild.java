a3d74fa4de0bd7e6edb02889ad701317e8200348
wuman0173 <bwu0173@yahoo.com>
2015-10-07 16:32:28 -0400

    Create theChild.java

---
+++ theChild.java
@@ -0,0 +1,15 @@
+/*
+Benjamin Wu
+CISC 3150
+HW5 Question 1
+*/
+package theChild;
+import parentpackage.ParentYO;
+
+public class theChild extends ParentYO
+{
+	public void printout()
+	{
+	System.out.println(x);
+	}	
+}
